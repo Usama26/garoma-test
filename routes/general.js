0d0d76d18a51b6af32f45c1fef75a9b77cf6488e
Usama26 <usama.waseem96@gmail.com>
2022-08-21 00:46:44 +0200

    same user and no user validation added

--- routes/general.js
+++ routes/general.js
@@ -15,11 +15,28 @@ let y = await User({name: "John" , email : "m@m.com" , password:"999"}).save();
 router.post('/bookMeeting', async (req, res) => {
     const validBooking = validateBooking(req.body);
   if (validBooking.error)
-    return res.status(status.BOOK_SERVICE_FAILED.code).json({
-      ...status.BOOK_SERVICE_FAILED,
+    return res.status(status.BOOK_MEETING_FAILED.code).json({
+      ...status.BOOK_MEETING_FAILED,
       message: validBooking.error.details[0].message,
     });
-    const { from_user, to_user, from_time, to_time, meeting_date } = req.body;
+
+    // if both ids are same then return error
+    if (req.body.from_user == req.body.to_user) {
+        return res.status(status.BOOK_MEETING_FAILED.code).json({
+            ...status.BOOK_MEETING_FAILED,
+            message: 'Both users cannot be same',
+        })};
+    
+    const users = await User.find({
+      _id: { $in: [req.body.data.from_user, req.body.data.to_user] },
+    });
+    if (users.length !== 2)
+      return res.status(status.BOOK_MEETING_FAILED.code).json({
+        ...status.BOOK_MEETING_FAILED,
+        message: 'No user found with the given id(s)',
+      });
+
+
     const booking = new Booking({ from_user, to_user, from_time, to_time, meeting_date });
     
     const savedBooking = await booking.save();
