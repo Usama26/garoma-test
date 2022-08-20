89e4873cd9c246de5c10d44b922a879107c6ba4c
Usama26 <usama.waseem96@gmail.com>
2022-08-20 22:28:47 +0200

    express setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const express = require('express');
+
+const app = express();
+
+//Middlewares
+app.use(express.json());
+
+
+// Setting up timezone
+const moment = require('moment-timezone');
+moment.tz.setDefault(process.env.TIMEZONE||"America/New_York");
+
+// mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('Connected to MongoDB...'))
+//   .catch(err => console.error('Could not connect to MongoDB...', err));
+
+app.listen(process.env.SERVER_PORT, () => console.log('Server Running On Port : ' + process.env.SERVER_PORT));
