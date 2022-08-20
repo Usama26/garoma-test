793d2e2d5f61ba05b34bd83dc46fe6f1c4b80c80
Usama26 <usama.waseem96@gmail.com>
2022-08-21 00:36:45 +0200

    validation added

--- routes/general.js
+++ routes/general.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { User } = require('../models/user');
 
+const {  validateBooking } = require('../validations/user');
+
 
 router.get("/", async (req, res) => {
  let x = await User.find()
@@ -10,7 +12,12 @@ let y = await User({name: "John" , email : "m@m.com" , password:"999"}).save();
 })
 
 router.post('/bookMeeting', async (req, res) => {
-  
+    const validBooking = validateBooking(req.body);
+  if (validBooking.error)
+    return res.status(status.BOOK_SERVICE_FAILED.code).json({
+      ...status.BOOK_SERVICE_FAILED,
+      message: validBooking.error.details[0].message,
+    });
     const { from_user, to_user, from_time, to_time, meeting_date } = req.body;
     const booking = new Booking({ from_user, to_user, from_time, to_time, meeting_date });
     
