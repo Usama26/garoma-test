7994822297bca59d13ab085668f3fa7ab4216f30
Usama26 <usama.waseem96@gmail.com>
2022-08-20 22:50:39 +0200

    connection to db created

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use('/api', general);
 const moment = require('moment-timezone');
 moment.tz.setDefault(process.env.TIMEZONE||"America/New_York");
 
-// mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('Connected to MongoDB...'))
-//   .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.listen(process.env.SERVER_PORT, () => console.log('Server Running On Port : ' + process.env.SERVER_PORT));
